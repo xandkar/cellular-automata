3968ca952b76e4a86d0fb866864f055a931552da
Siraaj Khandkar <siraaj@khandkar.net>
2012-08-09 02:19:25 -0400

    These fields should never be 0.

--- 003/src/life.erl
+++ 003/src/life.erl
@@ -12,10 +12,10 @@
 
 -record(state, {x            :: non_neg_integer()
                ,y            :: non_neg_integer()
-               ,n            :: non_neg_integer()
+               ,n            :: pos_integer()
                ,bar          :: string()
                ,board        :: array()
-               ,gen_count    :: non_neg_integer()
+               ,gen_count    :: pos_integer()
                ,gen_duration :: non_neg_integer()
                ,print_time   :: non_neg_integer()
                }).
