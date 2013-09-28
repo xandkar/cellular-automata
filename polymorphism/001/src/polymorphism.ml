ce139f1f2ee9903f6a1447e298da831acc4af9d1
Siraaj Khandkar <siraaj@khandkar.net>
2013-09-28 15:29:14 -0400

    Moved Terminal module to the top.
    So it can be visible from more modules.

--- polymorphism/001/src/polymorphism.ml
+++ polymorphism/001/src/polymorphism.ml
@@ -1,6 +1,39 @@
 open Core.Std
 
 
+module Terminal : sig
+  type color = [ `green
+               | `red
+               ]
+
+  val string_with_color : string -> color -> string
+
+  val clear : unit -> unit
+
+  val reset : unit -> unit
+end = struct
+  type color = [ `green
+               | `red
+               ]
+
+  let ansi_code_clear = "\027[2J"    (* Clear screen *)
+  let ansi_code_reset = "\027[1;1H"  (* Reset cursor position *)
+
+  let string_of_color = function
+    | `green -> "\027[0;32m"
+    | `red   -> "\027[1;31m"
+
+  let string_with_color s c =
+    sprintf "%s%s\027[0m" (string_of_color c) s
+
+  let clear () =
+    print_string ansi_code_clear
+
+  let reset () =
+    print_string ansi_code_reset
+end
+
+
 module type MATRIX = sig
   module Point : sig
     type t = {r : int; k : int}
@@ -194,39 +227,6 @@ module Conway : RULE = struct
     |> state_of_string
     |> next ~live_neighbors:(live_neighbors inputs)
     |> cell_of_state
-end
-
-
-module Terminal : sig
-  type color = [ `green
-               | `red
-               ]
-
-  val string_with_color : string -> color -> string
-
-  val clear : unit -> unit
-
-  val reset : unit -> unit
-end = struct
-  type color = [ `green
-               | `red
-               ]
-
-  let ansi_code_clear = "\027[2J"    (* Clear screen *)
-  let ansi_code_reset = "\027[1;1H"  (* Reset cursor position *)
-
-  let string_of_color = function
-    | `green -> "\027[0;32m"
-    | `red   -> "\027[1;31m"
-
-  let string_with_color s c =
-    sprintf "%s%s\027[0m" (string_of_color c) s
-
-  let clear () =
-    print_string ansi_code_clear
-
-  let reset () =
-    print_string ansi_code_reset
 end
 
 
